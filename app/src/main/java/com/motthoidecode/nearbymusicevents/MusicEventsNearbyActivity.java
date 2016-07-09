39b6c49df40fdcf09441b7b6ab5a545a19e11a84
Azure1609 <holuong1609@gmail.com>
2016-07-09 17:34:02 +0700

    Get Current Location

--- app/src/main/java/com/motthoidecode/nearbymusicevents/MusicEventsNearbyActivity.java
+++ app/src/main/java/com/motthoidecode/nearbymusicevents/MusicEventsNearbyActivity.java
@@ -1,12 +1,19 @@
 package com.motthoidecode.nearbymusicevents;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
+import android.location.Location;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import model.GoogleApiLocation;
 import utils.PreferenceUtils;
 
 public class MusicEventsNearbyActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
@@ -18,6 +25,8 @@ public class MusicEventsNearbyActivity extends AppCompatActivity implements Seek
             nr_radius_txt_4, nr_radius_txt_5, nr_radius_txt_6, nr_radius_txt_7;
 
     private PreferenceUtils mPreference;
+    private Location mCurrentLocation;
+    private GoogleApiLocation googleApiLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,20 +36,34 @@ public class MusicEventsNearbyActivity extends AppCompatActivity implements Seek
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        nr_radius_txt_0 = (TextView)findViewById(R.id.nr_radius_txt_0);
-        nr_radius_txt_1 = (TextView)findViewById(R.id.nr_radius_txt_1);
-        nr_radius_txt_2 = (TextView)findViewById(R.id.nr_radius_txt_2);
-        nr_radius_txt_3 = (TextView)findViewById(R.id.nr_radius_txt_3);
-        nr_radius_txt_4 = (TextView)findViewById(R.id.nr_radius_txt_4);
-        nr_radius_txt_5 = (TextView)findViewById(R.id.nr_radius_txt_5);
-        nr_radius_txt_6 = (TextView)findViewById(R.id.nr_radius_txt_6);
-        nr_radius_txt_7 = (TextView)findViewById(R.id.nr_radius_txt_7);
+        nr_radius_txt_0 = (TextView) findViewById(R.id.nr_radius_txt_0);
+        nr_radius_txt_1 = (TextView) findViewById(R.id.nr_radius_txt_1);
+        nr_radius_txt_2 = (TextView) findViewById(R.id.nr_radius_txt_2);
+        nr_radius_txt_3 = (TextView) findViewById(R.id.nr_radius_txt_3);
+        nr_radius_txt_4 = (TextView) findViewById(R.id.nr_radius_txt_4);
+        nr_radius_txt_5 = (TextView) findViewById(R.id.nr_radius_txt_5);
+        nr_radius_txt_6 = (TextView) findViewById(R.id.nr_radius_txt_6);
+        nr_radius_txt_7 = (TextView) findViewById(R.id.nr_radius_txt_7);
         radiusControl = (SeekBar) findViewById(R.id.nr_radius_bar);
         mPreference = new PreferenceUtils(getApplicationContext());
 
-        nr_radius_txt_2.setTextColor(Color.parseColor(ORANGE));
         radiusControl.setProgress(mPreference.getSeekBarProgress());
         radiusControl.setOnSeekBarChangeListener(this);
+
+        setRadiusTextColor();
+        googleApiLocation = new GoogleApiLocation(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        googleApiLocation.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        googleApiLocation.disconnect();
     }
 
     @Override
@@ -48,6 +71,58 @@ public class MusicEventsNearbyActivity extends AppCompatActivity implements Seek
         switch (i) {
             case 0:
                 mPreference.saveSearchRadius(1);
+                break;
+            case 1:
+                mPreference.saveSearchRadius(5);
+                break;
+            case 2:
+                mPreference.saveSearchRadius(10);
+                break;
+            case 3:
+                mPreference.saveSearchRadius(20);
+                break;
+            case 4:
+                mPreference.saveSearchRadius(50);
+                break;
+            case 5:
+                mPreference.saveSearchRadius(100);
+                break;
+            case 6:
+                mPreference.saveSearchRadius(200);
+                break;
+            case 7:
+                mPreference.saveSearchRadius(500);
+                break;
+            default:
+                setRadiusTextColor();
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+    }
+
+    public void getMusicEventsNearby() {
+        if (!isNetworkAvailable()) {
+            Toast.makeText(this, "Can not connect to the Internet!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        final ProgressDialog progressDialog = new ProgressDialog(MusicEventsNearbyActivity.this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+    }
+
+    public void setCurrentLocation(Location location) {
+        mCurrentLocation = location;
+    }
+
+    private void setRadiusTextColor() {
+        switch (mPreference.getSeekBarProgress()) {
+            case 0:
                 nr_radius_txt_0.setTextColor(Color.parseColor(ORANGE));
                 nr_radius_txt_1.setTextColor(Color.parseColor(GRAY));
                 nr_radius_txt_2.setTextColor(Color.parseColor(GRAY));
@@ -58,7 +133,6 @@ public class MusicEventsNearbyActivity extends AppCompatActivity implements Seek
                 nr_radius_txt_7.setTextColor(Color.parseColor(GRAY));
                 break;
             case 1:
-                mPreference.saveSearchRadius(5);
                 nr_radius_txt_0.setTextColor(Color.parseColor(GRAY));
                 nr_radius_txt_1.setTextColor(Color.parseColor(ORANGE));
                 nr_radius_txt_2.setTextColor(Color.parseColor(GRAY));
@@ -69,7 +143,6 @@ public class MusicEventsNearbyActivity extends AppCompatActivity implements Seek
                 nr_radius_txt_7.setTextColor(Color.parseColor(GRAY));
                 break;
             case 2:
-                mPreference.saveSearchRadius(10);
                 nr_radius_txt_0.setTextColor(Color.parseColor(GRAY));
                 nr_radius_txt_1.setTextColor(Color.parseColor(GRAY));
                 nr_radius_txt_2.setTextColor(Color.parseColor(ORANGE));
@@ -80,7 +153,6 @@ public class MusicEventsNearbyActivity extends AppCompatActivity implements Seek
                 nr_radius_txt_7.setTextColor(Color.parseColor(GRAY));
                 break;
             case 3:
-                mPreference.saveSearchRadius(20);
                 nr_radius_txt_0.setTextColor(Color.parseColor(GRAY));
                 nr_radius_txt_1.setTextColor(Color.parseColor(GRAY));
                 nr_radius_txt_2.setTextColor(Color.parseColor(GRAY));
@@ -91,7 +163,6 @@ public class MusicEventsNearbyActivity extends AppCompatActivity implements Seek
                 nr_radius_txt_7.setTextColor(Color.parseColor(GRAY));
                 break;
             case 4:
-                mPreference.saveSearchRadius(50);
                 nr_radius_txt_0.setTextColor(Color.parseColor(GRAY));
                 nr_radius_txt_1.setTextColor(Color.parseColor(GRAY));
                 nr_radius_txt_2.setTextColor(Color.parseColor(GRAY));
@@ -102,7 +173,6 @@ public class MusicEventsNearbyActivity extends AppCompatActivity implements Seek
                 nr_radius_txt_7.setTextColor(Color.parseColor(GRAY));
                 break;
             case 5:
-                mPreference.saveSearchRadius(100);
                 nr_radius_txt_0.setTextColor(Color.parseColor(GRAY));
                 nr_radius_txt_1.setTextColor(Color.parseColor(GRAY));
                 nr_radius_txt_2.setTextColor(Color.parseColor(GRAY));
@@ -114,7 +184,6 @@ public class MusicEventsNearbyActivity extends AppCompatActivity implements Seek
                 nr_radius_txt_7.setTextColor(Color.parseColor(GRAY));
                 break;
             case 6:
-                mPreference.saveSearchRadius(200);
                 nr_radius_txt_0.setTextColor(Color.parseColor(GRAY));
                 nr_radius_txt_1.setTextColor(Color.parseColor(GRAY));
                 nr_radius_txt_2.setTextColor(Color.parseColor(GRAY));
@@ -125,7 +194,6 @@ public class MusicEventsNearbyActivity extends AppCompatActivity implements Seek
                 nr_radius_txt_7.setTextColor(Color.parseColor(GRAY));
                 break;
             case 7:
-                mPreference.saveSearchRadius(500);
                 nr_radius_txt_0.setTextColor(Color.parseColor(GRAY));
                 nr_radius_txt_1.setTextColor(Color.parseColor(GRAY));
                 nr_radius_txt_2.setTextColor(Color.parseColor(GRAY));
@@ -138,11 +206,12 @@ public class MusicEventsNearbyActivity extends AppCompatActivity implements Seek
         }
     }
 
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
-    }
-
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
+    private boolean isNetworkAvailable() {
+        ConnectivityManager cm = (ConnectivityManager)
+                getSystemService(this.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        if (null == networkInfo)
+            return false;
+        return networkInfo.isConnectedOrConnecting();
     }
 }
---
+++ app/src/main/java/model/GoogleApiLocation.java
@@ -0,0 +1,186 @@
+package model;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.motthoidecode.nearbymusicevents.MusicEventsNearbyActivity;
+
+/**
+ * Created by Administrator on 7/9/2016.
+ */
+public class GoogleApiLocation implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    //Define a request code to send to Google Play services
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private LocationManager locationManager;
+
+    private Activity mActivity;
+
+    // flag for GPS status
+    boolean isGPSEnabled = false;
+
+    // flag for network status
+    boolean isNetworkEnabled = false;
+
+    public GoogleApiLocation(Activity activity){
+        this.mActivity = activity;
+        mGoogleApiClient = new GoogleApiClient.Builder(mActivity)
+                // The next two lines tell the new client that “this” current class will handle connection stuff
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                //fourth line adds the LocationServices API endpoint from GooglePlayServices
+                .addApi(LocationServices.API)
+                .build();
+
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
+
+        locationManager = (LocationManager) mActivity
+                .getSystemService(Context.LOCATION_SERVICE);
+
+        // getting GPS status
+        isGPSEnabled = locationManager
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        // getting network status
+        isNetworkEnabled = locationManager
+                .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if (!isGPSEnabled && !isNetworkEnabled) {
+            // no network provider is enabled
+            showSettingsAlert();
+        }
+
+    }
+
+    public void connect(){
+        //Now lets connect to the API
+        mGoogleApiClient.connect();
+    }
+
+    public void disconnect(){
+        //Disconnect from API onPause()
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    /**
+     * Function to show settings alert dialog
+     * On pressing Settings button will lauch Settings Options
+     * */
+    public void showSettingsAlert(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mActivity);
+
+        // Setting Dialog Title
+        alertDialog.setTitle("GPS is settings");
+
+        // Setting Dialog Message
+        alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
+
+        // On pressing Settings button
+        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog,int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                mActivity.startActivity(intent);
+            }
+        });
+
+        // on pressing cancel button
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        // Showing Alert Message
+        alertDialog.show();
+    }
+
+    /**
+     * If connected get current location
+     *
+     */
+    @Override
+    public void onConnected(Bundle bundle) {
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if (location == null) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+        } else {
+            //If everything went fine lets get current location
+            Toast.makeText(mActivity, "Current Location: "+location.getLatitude() + " - " + location.getLongitude() + "", Toast.LENGTH_SHORT).show();
+            MusicEventsNearbyActivity musicEventsNearbyActivity = (MusicEventsNearbyActivity)mActivity;
+            musicEventsNearbyActivity.setCurrentLocation(location);
+            musicEventsNearbyActivity.getMusicEventsNearby();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+             * Google Play services can resolve some errors it detects.
+             * If the error has a resolution, try sending an Intent to
+             * start a Google Play services activity that can resolve
+             * error.
+             */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(mActivity, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                    /*
+                     * Thrown if Google Play services canceled the original
+                     * PendingIntent
+                     */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+                /*
+                 * If no resolution is available, display a dialog to the
+                 * user with the error.
+                 */
+            Log.e("Error", "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+    /**
+     * If locationChanges change current location
+     *
+     *
+     * @param location
+     */
+    @Override
+    public void onLocationChanged(Location location) {
+        ((MusicEventsNearbyActivity)mActivity).setCurrentLocation(location);
+    }
+}
